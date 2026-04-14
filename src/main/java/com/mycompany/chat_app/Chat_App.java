aefdacc5db40b29b6769a3ed9c13c9b49963e656
ST10511158 <ST10511158@myemeris.edu.za>
2026-04-14 20:02:57 +0200

    added files to github
    First time moving files to github

---
+++ src/main/java/com/mycompany/chat_app/Chat_App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chat_app;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Chat_App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
